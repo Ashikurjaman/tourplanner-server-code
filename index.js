55c56b2478dda03cd632c925a5f04b46bede7179
Ashikurjamane <ashikmpi420@gmail.com>
2021-11-19 19:29:09 +0600

    setplan

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
             res.json(service)
         })
 
+        app.get("/packages/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await packageCollection.findOne(query);
+            res.json(product);
+          });
+
 
     }
     finally{
